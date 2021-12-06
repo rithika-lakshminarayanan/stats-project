61c631c4f7f08704c18fd3b4d2cb06f35109c9d1
Rithika Lakshminarayanan <lakshminarayanan.r@husky.neu.edu>
2021-12-06 12:42:22 -0500

    Added number of hours of sleep column to CSV

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# stats-project" 
+# Analyzing the relationship between sleep, type of EMA and number of EMA prompts answered
+
+This repo contains scripts used to statistically analyze the relationship between sleep, type of EMA and number of EMA prompts answered in a longitudinal study. Due to the sensitive nature of the data provided by participants, we use simulated data here.
+
--- generate_data.py
+++ generate_data.py
@@ -10,7 +10,7 @@ def append_list_as_row(file_name, list_of_elem):
         # Add contents of list as last row in the csv file
         csv_writer.writerow(list_of_elem)
 
-row_contents = ['participant_id', 'day', 'prompts', 'answers', 'ema']  
+row_contents = ['participant_id', 'day', 'prompts', 'answers', 'sleep_hours', 'ema']  
 
 append_list_as_row('participant_data.csv', row_contents)
 
@@ -29,19 +29,24 @@ while participant_id <= 100:
 
     s_prompts = np.random.normal(mu, sigma, 30)
 
-    
     s_answers = np.random.binomial(n, p, 30)
 
-    ema_options = [0,1]
+    sleep_hours = np.random.uniform(4.5, 10.6, 30)
+
+    round_sleep = [round(num, 1) for num in sleep_hours]
+
+    ema = np.random.randint(2, size=30)
 
     for i in range(30):
         p_list = []
+
 
         p_list.append(participant_id)
         p_list.append(i+1)
         p_list.append(int(round(s_prompts[i])))
         p_list.append(s_answers[i])
-        p_list.append(random.choice(ema_options))
+        p_list.append(round_sleep[i])
+        p_list.append(ema[i])
 
         append_list_as_row('participant_data.csv', p_list)
 
